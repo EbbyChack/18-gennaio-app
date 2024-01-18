aef686503f98e21671591976406d4dbf67bd5592
Ebby Chakkungal <121340114+vlirkbog@users.noreply.github.com>
2024-01-18 17:06:20 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <MyNavbar/>
         <Routes>
-          <Route path="/" element={<Articles />}/>
+          <Route path="/home" element={<Articles />}/>
           <Route path="/article/:id" element={<Article />}/>
         </Routes>
       </BrowserRouter>
--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom";
 
 const MyNavbar = () => {
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Spaceflight</a>
+        <Link className="navbar-brand" to="/home">Spaceflight</Link>
       </div>
     </nav>
   );
