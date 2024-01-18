8e005730e9d4c15f52c6632b016f74d110124bd5
Ebby Chakkungal <121340114+vlirkbog@users.noreply.github.com>
2024-01-18 16:39:10 +0100

    ok

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -7,6 +7,8 @@ const Article = () => {
     title: string;
     summary: string;
     image_url: string;
+    url: string;
+    news_site: string;
   }
   const [article, setArticle] = useState<Article | undefined>();
 
@@ -18,6 +20,7 @@ const Article = () => {
         const response = await fetch("https://api.spaceflightnewsapi.net/v4/articles/" + `${id}`);
         const data = await response.json();
         setArticle(data);
+        console.log(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -27,9 +30,29 @@ const Article = () => {
   }, [id]);
 
   return (
-    <div>
+    <div >
       {article ? (
-        <div></div>
+        <div className="container my-3 ">
+          <div className="row">
+            <div className="col-8 mx-auto">
+              <div className="card-container ">
+                <div className="card ">
+                  <img
+                    src={article.image_url}
+                    className="card-img-top"
+                    alt={article.title}
+                    style={{ objectFit: "cover" }}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{article.title}</h5>
+                    <p className="card-text ">{article.summary}</p>
+                    <button className="btn btn-light"><a href={article.url} target="_blank" >Read more...</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
         <div>Loading...</div>
       )}
